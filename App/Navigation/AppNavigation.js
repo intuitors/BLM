56314f7a32a0da3e56e5b916e16f7c394a533706
Thashitharan <thashitharan.r@gmail.com>
2017-11-17 10:33:46 +0530

    first commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,10 +1,14 @@
 import { StackNavigator } from 'react-navigation'
+import ProfessionalScreen from '../Containers/ProfessionalScreen'
+import NavigationScreen from '../Containers/NavigationScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  ProfessionalScreen: { screen: ProfessionalScreen },
+  NavigationScreen: { screen: NavigationScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
